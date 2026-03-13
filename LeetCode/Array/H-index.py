04c62c2addae9c7619aa65550a3b8a528b53b936
Aarya Upadhyay <aaryau.2005@gmail.com>
2026-03-13 18:49:05 +0530

    Add hIndex function to calculate H-index
    Implement hIndex function to calculate H-index from citations.

---
+++ LeetCode/Array/H-index.py
@@ -0,0 +1,19 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        """n=len(citations)
+        cited_count=0
+        for i in range(n+1):
+            c=0
+            for j in range(n):
+                if i<=citations[j]:
+                    c+=1
+            if i<=c:
+               cited_count=i
+        return cited_count  """
+        citations.sort()
+        n=len(citations)
+        
+        for i in range(n):
+            if citations[i]>=n-i:
+                return n-i
+        return 0
