e3cd775e468940ece43e9605c22faa1e5593ce97
Aarya Upadhyay <aaryau.2005@gmail.com>
2026-03-14 11:29:29 +0530

    Create insert delete getrandom O(1).py

---
+++ LeetCode/Array/insert delete getrandom O(1).py
@@ -0,0 +1,35 @@
+import random
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.nums = []          # list to store elements
+        self.pos = {}           # hashmap: value → index
+
+    def insert(self, val: int) -> bool:
+        if val in self.pos:
+            return False
+        
+        self.nums.append(val)
+        self.pos[val] = len(self.nums) - 1
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.pos:
+            return False
+        
+        index = self.pos[val]
+        last_val = self.nums[-1]
+
+        # swap with last element
+        self.nums[index] = last_val
+        self.pos[last_val] = index
+
+        # remove last
+        self.nums.pop()
+        del self.pos[val]
+
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.nums)
